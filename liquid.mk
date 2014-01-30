9b1a15393970372f153bc863c31133ff5e57d4a6
Sean Hoyt <seanhoyt963@gmail.com>
2014-01-30 08:24:00 -0600

    Remove common_cdma

--- liquid.mk
+++ liquid.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 
-$(call inherit-product, vendor/liquid/config/common_cdma.mk)
-
 $(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wls BUILD_ID=JSS15J BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.3/JSS15J/251863.2:user/release-keys" PRIVATE_BUILD_DESC="3.04.651.2 CL251863 release-keys"
