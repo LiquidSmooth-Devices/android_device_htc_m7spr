88242c9f0f6dae1920d6424e24751a8a319d59f0
Ethan <cannondalev2000@gmail.com>
2015-05-16 13:07:59 -0400

    fix merge derpage

--- device.mk
+++ device.mk
@@ -17,21 +17,11 @@
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/htc/m7spr/m7spr-vendor.mk)
 
-# local overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+# overlays
+DEVICE_PACKAGE_OVERLAYS += device/htc/m7spr/overlay
 
-<<<<<<< HEAD
-# OTA ID
-PRODUCT_PROPERTY_OVERRIDES += \
-    otaupdater.otaid=liquidm7spr
-=======
-# FM radio
-PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
->>>>>>> 8bcc31d8faacaa958d3a350b1e334022728766db
+# common overlays
+DEVICE_PACKAGE_OVERLAYS += device/htc/m7-common/overlay-cdma
 
 # Inherit from m7-common
 $(call inherit-product, device/htc/m7-common/m7-common.mk)
